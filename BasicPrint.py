080ed85b3fc11a8d91dcfee8223193d993197bae
Anurag <Anuragkanade6@gmail.com>
2023-03-03 00:07:15 +0530

    Day1

---
+++ BasicPrint.py
@@ -0,0 +1,3 @@
+# printing a simple line 
+# Practice statement for print function
+print('Welcome to 100 Days of code challenge')
---
+++ isVowelorConsonant.py
@@ -0,0 +1,28 @@
+# This code was written by @Anurag_Kanade
+
+'''Day 1 coding Statement: Write a program to identify if the character is a vowel or consonant.
+Description of the program: 
+Take an input character from the user and check whether the given input is a vowel or consonant.
+Input
+A
+Output
+Vowel'''
+
+
+vowel = ['A','E','I','O','U','a','e','i','o','u']
+conso = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+
+isVowel = input('Enter a Character: ')
+
+# Here checking if entered character is vowel or an consonant or neither of it.  
+if isVowel in vowel:
+    print('Vowel')
+elif isVowel in conso:
+    print('Consonant')
+else:
+    print('Invalid Input')
+
+
+
+
