4d1964e583a3e06a0f206fca60a211cffb96c450
Anurag <Anuragkanade6@gmail.com>
2023-03-04 22:06:42 +0530

    removed trailing spaces

--- Day3.py
+++ Day3.py
@@ -1,6 +1,11 @@
 # This code was written by @Anurag_Kanade on 3 march 2023
 
-'''  '''
-A_char = input('Check ASCII value of: ')
-print(ord(A_char))
+'''Day 3 coding Statement: Write a program to find ASCII values of a character
 
+Description:
+
+Get an input character from the user and give the ASCII value of the given input as the output. '''
+
+A_char = input('Check ASCII value of: ')
+
+print(ord(A_char))
