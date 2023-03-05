48eb19980c29c2f58b94d863283b82379ea0db5c
Anurag <Anuragkanade6@gmail.com>
2023-03-05 19:43:54 +0530

    Day 4

---
+++ Day4.py
@@ -0,0 +1,19 @@
+# This code was written by @Anurag_Kanade on 5 march 2023
+
+'''Day 4 coding Statement:  Write a program to identify of the a number is positive or negative
+
+Description
+
+Get an input number from the user and check whether it is a positive or negative number.
+
+Input :
+
+'''
+value = int(input("Enter a Number: "))
+if value>0:
+    print("Positive number")
+elif value<0:
+    print("Negative number")
+else:
+    print("Neither positive nor negative")
+
