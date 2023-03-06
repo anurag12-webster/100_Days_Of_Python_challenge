0e95431bb722f933fbac9ecc555b104314ccb182
Anurag <Anuragkanade6@gmail.com>
2023-03-06 23:38:20 +0530

    Day 5

---
+++ Day5.py
@@ -0,0 +1,13 @@
+# This code was written by @Anurag_Kanade on 6 march 2023
+
+'''Day 5 coding Statement:  Write a program to identify if the number is even or odd
+
+Description
+
+Get a number as input from the user and check whether the given number is odd or even
+'''
+Number = int(input('Enter a value: '))
+if Number %2==0:
+    print('Even')
+else:
+    print('Odd')
