27a1e42187f3d49ed76f43b12c4282fc8f9a39f6
Anurag <Anuragkanade6@gmail.com>
2023-03-19 10:50:51 +0530

    Day 13

--- Day13.py
+++ Day13.py
@@ -1,3 +1,6 @@
+#  This code was written by @Anurag_Kanade on 15 march 2023
+'''Day 12 coding Statement:  Write a program to find Sum of n natural numbers.'''
+
 n = int(input("Enter the value of n: "))
 sum = 0
 
